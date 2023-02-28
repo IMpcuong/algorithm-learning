050b4171e3dfb36fd805961a6c2b3c4c64a189be
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-02-28 22:46:37 +0700

    chore: Golang using camel-case convention instead of snake-case

--- search/BinarySearch.go
+++ search/BinarySearch.go
@@ -3,7 +3,7 @@ package main
 import "fmt"
 
 // Verbose implementation:
-func binary_search_v1(num int, arr []int) int {
+func binarySearchV1(num int, arr []int) int {
 	low := 0
 	high := len(arr) - 1
 
@@ -21,7 +21,7 @@ func binary_search_v1(num int, arr []int) int {
 	return -1
 }
 
-func binary_search_v2(num int, arr []int) int {
+func binarySearchV2(num int, arr []int) int {
 	low := 0
 	high := len(arr) - 1
 
@@ -38,15 +38,15 @@ func binary_search_v2(num int, arr []int) int {
 	return -1
 }
 
-func binary_search_recursive(num, low, high int, arr []int) int {
+func binarySearchRecursive(num, low, high int, arr []int) int {
 	for low <= high {
 		mid := (low + high) / 2
 		guess := arr[mid]
 		if guess > num {
-			binary_search_recursive(num, low, mid-1, arr)
+			binarySearchRecursive(num, low, mid-1, arr)
 		}
 		if guess < num {
-			binary_search_recursive(num, mid+1, high, arr)
+			binarySearchRecursive(num, mid+1, high, arr)
 		}
 		return mid
 	}
@@ -55,7 +55,7 @@ func binary_search_recursive(num, low, high int, arr []int) int {
 
 func main() {
 	arr := []int{0, 2, 4, 5, 7, 9, 11, 12, 13, 18}
-	fmt.Println(binary_search_v1(7, arr), arr[4])
-	fmt.Println(binary_search_v2(7, arr), arr[4])
-	fmt.Println(binary_search_recursive(7, 0, len(arr)-1, arr), arr[4])
+	fmt.Println(binarySearchV1(7, arr), arr[4])                       // Idx: 4, Val: 7.
+	fmt.Println(binarySearchV2(7, arr), arr[4])                       // Idx: 4, Val: 7.
+	fmt.Println(binarySearchRecursive(7, 0, len(arr)-1, arr), arr[4]) // Idx: 4, Val: 7.
 }

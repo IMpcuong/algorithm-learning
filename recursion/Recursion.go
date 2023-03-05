d725fb94875b164d0f9cb81fb82ee5469c67ca4b
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-03-05 22:06:27 +0700

    learn(recursion+dac): Divide-and-conquer imitation with `math.Pow`

--- recursion/Recursion.go
+++ recursion/Recursion.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"strconv"
 )
 
@@ -22,14 +23,34 @@ func greatestCommonDivisor(a, b int) int {
 	return greatestCommonDivisor(b, r)
 }
 
+// NOTE: `pow(x, n)`.
+// The time complexity: `O(log n)`
+func pow(x float64, n int) float64 {
+	if n == 0 {
+		return 1
+	}
+	// NOTE: `pow(x, -n)` := `1 / (x^n)`.
+	if n < 0 {
+		n = -1 * n
+		x = 1 / x
+	}
+	if n%2 == 0 {
+		return pow(x*x, n/2) // `(x^2)^(n/2)` := `x^(2*n/2)` := `x^n`.
+	} else {
+		return x * pow(x, n-1) // `x * x^(n-1)` := `x^n`.
+	}
+}
+
 func main() {
 	convertToASCII(0x80)
 
 	arr := []int{1, 2, 3, 4, 5}
 	sum := SumDAC(arr)
-	fmt.Println(sum)
+	fmt.Println(sum) // 15.
 
 	fmt.Println(len(arr) == CountDAC(arr), len(arr) == CountElement(arr))
 
-	fmt.Println(greatestCommonDivisor(225, 125))
+	fmt.Println(greatestCommonDivisor(225, 125)) // 25.
+
+	fmt.Println(pow(-2, 10) == math.Pow(-2, 10)) // 1024.
 }

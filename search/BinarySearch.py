996f60975c86eac5d1eff87bec03b8c9a1f794d6
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-05-23 17:38:01 +0700

    feat: Using Python instead of Golang due to the diversity of DataStructure for this language

---
+++ search/BinarySearch.py
@@ -0,0 +1,41 @@
+import pprint
+import typing
+from collections import deque
+
+
+def init_graph() -> typing.Dict[str, typing.List[str]]:
+    graph = {}
+    graph["you"] = ["alice", "bob", "claire"]
+    graph["bob"] = ["anuj", "peggy"]
+    graph["alice"] = ["peggy"]
+    graph["claire"] = ["thom", "jonny"]
+    graph["anuj"] = []
+    graph["peggy"] = []
+    graph["thom"] = []
+    graph["jonny"] = []
+    return graph
+
+
+def bfs_seller(graph_neighbor: typing.Dict[str, typing.List[str]]) -> str:
+    search_queue = deque()
+    search_queue += graph_neighbor["you"]
+    searched: typing.List[str] = []
+    while search_queue:
+        person = search_queue.popleft()
+        if person not in searched and is_seller(person):
+            return person
+        else:
+            search_queue += graph_neighbor[person]
+            searched.append(person)
+    return str.__init__
+
+
+def is_seller(name: str) -> bool:
+    # NOTE(impcuong): The rightmost character from our friend's name must be letter "m"
+    #   to indicate that they're a seller.
+    return name[-1] == "m"
+
+
+if __name__ == "__main__":
+    seller = bfs_seller(init_graph())
+    pprint.pprint("Seller: " + seller)

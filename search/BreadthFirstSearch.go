214b652e5aa4dbe09abf0fead92b03c1594e3c91
IMpcuong <trinhtiencuong_t63@hus.edu.vn>
2023-05-23 17:36:46 +0700

    chore: Examine to perpetually use Golang as our main language -> but nah

--- search/BreadthFirstSearch.go
+++ search/BreadthFirstSearch.go
@@ -73,15 +73,23 @@ func exampleDirectedGraph() {
 	g.AddVertex(1)
 	g.AddVertex(2)
 	g.AddVertex(3)
+	g.AddVertex(4)
+	g.AddVertex(5)
+	g.AddVertex(6)
+	g.AddVertex(7)
+	g.AddVertex(8)
 
 	g.AddEdge(1, 2)
 	g.AddEdge(1, 3)
+	g.AddEdge(1, 4)
 
-	g.AddEdge(2, 1)
-	g.AddEdge(2, 3)
+	g.AddEdge(2, 5)
+	g.AddEdge(2, 6)
 
-	g.AddEdge(3, 1)
-	g.AddEdge(3, 2)
+	g.AddEdge(3, 6)
+
+	g.AddEdge(4, 7)
+	g.AddEdge(4, 8)
 
 	g.Print()
 }
